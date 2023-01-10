5a0b70db1f13b887db7da55e0e444852850cff7d
Phong Cao <phongcao0714@gmail.com>
2023-01-10 12:57:44 -0800

    Fix test connection

--- src/services/testService.js
+++ src/services/testService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const testCreatePostButton = () => {
     const promise = axios
-    .get('http://localhost:3001/api/notes')
+    .get('http://localhost:3001/api/notes/63bdca8b994a9da4b71c61e4')
     .then(response => {        
         console.log('promise fulfilled')
         console.log(response.data)    
