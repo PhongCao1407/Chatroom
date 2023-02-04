cca09f442f5188279dcf06a0e695063829f702f3
Phong Cao <phongcao0714@gmail.com>
2023-02-03 16:27:19 -0800

    Add google auth login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Thread } from './components/thread/thread';
 import { Header } from './components/header/header';
 
-
 function App() {
   return (
     <div className="App">
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,10 +3,81 @@ import './header.css'
 import logo from './static/Logo.svg';
 import home from './static/Home.png';
 import downArrow from './static/DownArrow.svg';
-import user from './static/User.png';
+import userImg from './static/User.png';
 import setting from './static/Setting.svg';
 
+import { useEffect, useState } from 'react'
+import jwt_decode from 'jwt-decode'
+
+import { clientID } from '../../googleClient';
+
 const Header = () => {
+    const [ user, setUser ] = useState({})
+
+    //Get call when signing in
+    const handleCallbackResponse = (response) => {
+        console.log(response)
+        console.log("Encoded JWT ID token: " + response.credential)
+        let userObject = jwt_decode(response.credential)
+
+        setUser(userObject)
+
+        // Hide google button
+        const loginButton = document.getElementById("login-button")
+        loginButton.style.display = "none"
+
+        //Fix layout and show username and settings
+        const header = document.getElementsByTagName("header")[0]
+        header.style.gridTemplateColumns = "1fr 2fr 4fr 1fr 1fr"
+
+        console.log(userObject)
+        const userInfo = document.getElementById("user-info")
+        userInfo.style.display = "flex"
+
+        const username = document.getElementById("username")
+        username.textContent = userObject.name
+
+      }
+
+      //Get call when signing in
+      const handleSignout = () => {
+        //Delete user from current session
+        setUser({})
+
+        //Fix stylings
+        const loginButton = document.getElementById("login-button")
+        loginButton.style.display = "flex"
+
+        const header = document.getElementsByTagName("header")[0]
+        header.style.gridTemplateColumns = "1fr 2fr 4fr 2fr"
+
+        const userInfo = document.getElementById("user-info")
+        userInfo.style.display = "none"
+
+        const username = document.getElementById("username")
+        username.textContent = ""
+      }
+    
+      //Render google sign in button
+      useEffect(() => {   
+        const google = window.google
+
+        //Setup google client
+        google.accounts.id.initialize({
+          client_id: clientID,
+          callback: handleCallbackResponse
+        })
+    
+        //Render button
+        google.accounts.id.renderButton(
+          document.getElementById("login-button"), 
+          {
+            theme: "outline",
+            size: "large",
+          }
+        )
+      }, [])
+
     return (
       <header>
         <img src={logo} alt="" className="logo"/>
@@ -22,17 +93,21 @@ const Header = () => {
                 <input type="text" placeholder="Search.." className="search-bar-input"/>
             </div>
         </div>
-        <div class="user-auth">
-          <button>Login</button>
+        <div className="user-auth" id="login-button">
         </div>
-        {/* <div className="user-info">
-            <img src={user} alt=""/>
+        <div className="user-info" id="user-info">
+            <img src={userImg} alt=""/>
             <div className="user-info-bar">
-                <p>Username123456</p>
+                <p id="username"></p>
             </div>
         </div>
-        <div className="settings">
-            <img src={setting} alt=""/>
+        {//Make sure the log out is displayed only when the user is logged in
+            Object.keys(user).length != 0 &&
+            <button id="logout-button" onClick={(e) => handleSignout(e)}>Sign Out</button>
+        }
+        
+        {/* <div className="settings" >
+            <img src={setting} id="settings" alt=""/>
         </div> */}
       </header>
     )
